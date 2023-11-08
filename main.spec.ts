c1091002dc86413bd714b257086efb4173a6afcd
vykiiTor <vykiitor@gmail.com>
2023-11-08 10:44:05 +0100

    test premier c branche

--- main.spec.ts
+++ main.spec.ts
@@ -2,7 +2,7 @@ import mainFunction from "./main";
 import {expect, jest, test} from '@jest/globals';
 
 describe('testing plus operation', () => {
-    test('3+2 string should result in 5', () => {
+    test('3 2 + string should result in 5', () => {
       expect(mainFunction("3 2 +")).toBe(5);
     });
   });
