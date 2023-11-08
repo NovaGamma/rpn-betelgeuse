2049f7237b7ea89cd879f0b5963617bc5dbfbac0
victor tran <48139447+vykiiTor@users.noreply.github.com>
2023-11-08 10:28:19 +0100

    test 1 et 3 passent

--- main.spec.ts
+++ main.spec.ts
@@ -2,13 +2,20 @@ import mainFunction from "./main";
 
 describe('testing plus operation', () => {
     test('3+2 string should result in 5', () => {
-      expect(mainFunction("3+2")).toBe(5);
+      expect(mainFunction("3 2 +")).toBe(5);
     });
   });
 
-describe('testing plus operation', () => {
-    test('3+2 string should result in 5', () => {
-        expect(mainFunction("3+2")).toBe(5);
+  describe('testing error empty', () => {
+    test('2 string should fail', () => {
+      expect(mainFunction("2")).toBe("jsp a voir apres");
+    });
+  });
+
+describe('testing sqrt operation', () => {
+    test('9 sqrt should result in 3', () => {
+        expect(mainFunction("9")).toBe(3);
     });
 });
+
 
